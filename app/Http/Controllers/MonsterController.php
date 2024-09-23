ed2e53b792f20b4289b9ec15add93e33f8b0830b
Kharen Aguiar <kharenaguiar@gmail.com>
2024-09-23 20:17:21 -0300

    Controle de monstro
    A mesma ideia do controlador de missões, pega os dados e manda pra exibição

--- app/Http/Controllers/MonsterController.php
+++ app/Http/Controllers/MonsterController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class MonsterController extends Controller
 {
-    //
+    public function index()
+    {
+        $monstros = Monstro::all(); // Busca todos os monstros do banco de dados
+        return view('bestiario.index', compact('monstros'));
+    }
+
 }
