cb8ae311909d67593c4063c02487ef1dd2781f0b
Kharen Aguiar <kharenaguiar@gmail.com>
2024-09-23 20:16:44 -0300

    Controlador de missão
    Controlador serve pra ler dados da missão e retornar para a view

--- app/Http/Controllers/MissionController.php
+++ app/Http/Controllers/MissionController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\HistoricoMissao;
 use Illuminate\Http\Request;
 
 class MissionController extends Controller
 {
-    //
+    public function index()
+    {
+        // Busca todas as missões do banco de dados
+        $missoes = HistoricoMissao::all();
+
+        // Retorna a view e passa os dados das missões para ela
+        return view('mission.historico_missoes', compact('missoes'));
+    }
 }
