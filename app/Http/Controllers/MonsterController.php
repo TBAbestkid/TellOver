35c82e06364e016a7a3b1e91fb5a336cc27181f5
Kharen Aguiar <kharenaguiar@gmail.com>
2024-11-04 18:56:59 -0300

    Update MonsterController.php
    Removendo então a busca de monstros e deixando apenas pra exibição

--- app/Http/Controllers/MonsterController.php
+++ app/Http/Controllers/MonsterController.php
@@ -8,8 +8,7 @@ class MonsterController extends Controller
 {
     public function index()
     {
-        $monstros = Monstro::all(); // Busca todos os monstros do banco de dados
-        return view('bestiario.index', compact('monstros'));
+        return view('bestiario.bestiario');
     }
 
 }
