8f5a6abe06a3ffd7df1f5573bdd1f34df6a9e885
Kharen Aguiar <kharenaguiar@gmail.com>
2024-11-04 18:56:33 -0300

    Removendo itens e adicionando função
    Removendo a variavel de pegar todo o historico, sendo que ainda não há nada no banco de dados, então é necessario só exibir, enfim, adicionando uma nova função que futuramente terá que criar missão, mas por enquanto só exibindo mesmo

--- app/Http/Controllers/MissionController.php
+++ app/Http/Controllers/MissionController.php
@@ -10,9 +10,12 @@ class MissionController extends Controller
     public function index()
     {
         // Busca todas as missões do banco de dados
-        $missoes = HistoricoMissao::all();
+        // $missoes = HistoricoMissao::all();
 
         // Retorna a view e passa os dados das missões para ela
-        return view('mission.historico_missoes', compact('missoes'));
+        return view('misson.historico_missoes');
+    }
+    public function criarMission(){
+        return view('misson.mission');
     }
 }
