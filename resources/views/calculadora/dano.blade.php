40b951b036bc2b62f354c54ab82be3c18e760b5e
Kharen Aguiar <kharenaguiar@gmail.com>
2025-06-17 15:14:53 -0300

    Update dano.blade.php

--- resources/views/calculadora/dano.blade.php
+++ resources/views/calculadora/dano.blade.php
@@ -78,7 +78,7 @@
     </form>
     <div id="result" class="mt-4 text-center"></div>
 </div>
-
+<!-- Adicao de coment -->
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/5.1.3/js/bootstrap.bundle.min.js"></script>
 
