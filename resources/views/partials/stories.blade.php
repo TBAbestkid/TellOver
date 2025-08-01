8b4f8568b58256ece511e6360665f11d627e3756
Kharen Aguiar <149444184+TBAbestkid@users.noreply.github.com>
2025-08-01 14:29:37 -0300

    Visual de como srão os stories na parte de home

---
+++ resources/views/partials/stories.blade.php
@@ -0,0 +1,8 @@
+<div class="d-flex overflow-auto mb-3">
+    {{-- @foreach ($stories as $story) --}}
+        <div class="text-center me-3">
+            <img src="" class="rounded-circle border border-primary" width="64" height="64" alt="Story">
+            <div class="small mt-1"></div>
+        </div>
+    {{-- @endforeach --}}
+</div>
