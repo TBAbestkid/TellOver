623cf3cb28235ae2665c34ae2ad4e6c415c22eb0
Kharen Aguiar <149444184+TBAbestkid@users.noreply.github.com>
2025-08-01 13:48:15 -0300

    Adicionando novas rotas para o Web
    Como a parte de RPG foi removida, não há necessidade de rotas para paginas que não existem mais.
    No entanto agora há rota para login/cadstro com Google, além de rotas para notificações, ajustes na rota de perfil e muito mais.

--- routes/web.php
+++ routes/web.php
@@ -7,16 +7,28 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AccountController;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\MonsterController;
-use App\Http\Controllers\MissionController;
-use App\Http\Controllers\PersonagemController;
-use App\Http\Controllers\DamageCalculatorController;
-use App\Http\Controllers\WorldController;
 use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Response;
+use App\Http\Controllers\Auth\GoogleController;
+use App\Http\Controllers\NotificationController;
+use Illuminate\Support\Facades\Mail;
 
-Route::get('/', function () {
-    return view('home');
+Route::get('auth/google', [GoogleController::class, 'redirectToGoogle'])->name('google.login');
+Route::get('auth/google/callback', [GoogleController::class, 'handleGoogleCallback']);
+
+Route::get('/teste-email', function () {
+    $user = Auth::user();
+
+    if (!$user) {
+        return 'Usuário não autenticado.';
+    }
+
+    Mail::raw('Teste de email via Mailtrap', function ($message) use ($user) {
+        $message->to($user->email)
+                ->subject('Email de teste');
+    });
+
+    return 'Email enviado para ' . $user->email;
 });
 
 Auth::routes();
@@ -26,16 +38,23 @@ Route::post('/login', [LoginController::class, 'login']);
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
 // Rota para a página inicial (posts)
+Route::redirect('/', '/home');
 Route::get('/home', [PostController::class, 'index'])->name('home');
+
+// Notificações
+Route::get('/notifications', [NotificationController::class, 'index'])->name('notifications.index');
+Route::post('/notifications/{id}/read', [NotificationController::class, 'markAsRead'])->name('notifications.markAsRead');
+Route::post('/notifications/read-all', [NotificationController::class, 'markAllAsRead'])->name('notifications.markAllAsRead');
+
+Route::get('/sobre', [HomeController::class, 'about'])->name('about');
 
 // Rota para armazenar posts
-Route::post('/posts', [PostController::class, 'store'])->middleware('auth');
-
-// Recursos para edição, atualização e exclusão de posts
+Route::post('/posts', [PostController::class, 'store'])->middleware(['auth', 'validate.post']);
 Route::resource('posts', PostController::class)->except(['index']);
+Route::get('/post/{id}', [PostController::class, 'show'])->name('post.show');
 
-// Rota principal para exibir posts na home
-Route::get('/home', [PostController::class, 'index'])->name('home');
+// Perfil público por username
+Route::get('/perfil/{identificador}', [ProfileController::class, 'show'])->name('profile.show');
 
 // Página de configurações de conta
 Route::get('/account/settings', [AccountController::class, 'settings'])->name('account.settings');
@@ -46,111 +65,8 @@ Route::put('/account/update', [AccountController::class, 'update'])->name('accou
 // para as futuras pesquisas nér
 // Route::get('/search', [SearchController::class, 'search'])->name('search');
 
-Route::get('/profile', [ProfileController::class, 'show'])->name('profile.show');
-
 Route::get('/u/{username}', [ProfileController::class, 'showByUsername'])->name('profile.showByUsername');
 
 Route::post('/follow/{id}', [ProfileController::class, 'toggleFollow'])->name('profile.toggleFollow');
 
 Route::post('/profile/bio', [ProfileController::class, 'updateBio'])->name('profile.updateBio');
-
-
-Route::middleware('auth')->group(function() {
-    // Página de listagem de personagens
-    Route::get('/personagem', [PersonagemController::class, 'index'])->name('personagem');
-
-    // Página para criar um personagem
-    Route::get('/personagens/criar', [PersonagemController::class, 'create'])->name('criarpersonagem');
-
-    // Rota para o envio do formulário de criação de personagem
-    Route::post('/criar-personagem', [PersonagemController::class, 'criarPersonagem'])->name('criarpersonagem.post');
-
-    Route::get('/personagem/{personagem}/edit', [PersonagemController::class, 'edit'])->name('personagem.edit');
-
-    Route::put('/personagem/{personagem}', [PersonagemController::class, 'update'])->name('personagem.update');
-
-    Route::delete('/personagem/{personagem}', [PersonagemController::class, 'destroy'])->name('personagem.destroy');
-
-    // Rota para exibir inventário do personagem
-    Route::get('personagem/{personagem}/inventory', [PersonagemController::class, 'showInventory'])->name('personagem.inventory');
-
-    // Rota para equipar um item
-    Route::post('personagem/{personagem}/equip', [PersonagemController::class, 'equipItem'])->name('equipItem');
-
-    // Rota para adicionar item ao inventário
-    Route::post('personagem/{personagem}/add-item', [PersonagemController::class, 'addItemToInventory'])->name('addItemToInventory');
-    // Rota para exibir a página de seleção de personagens (GET)
-    Route::get('/personagem/selecionar', [PersonagemController::class, 'showCharacterSelection'])->name('personagem.selecao');
-
-    // Rota para selecionar um personagem (POST)
-    Route::post('/personagem/selecionar/{id}', [PersonagemController::class, 'select'])->name('personagem.select');
-
-});
-
-Route::middleware('auth')->group(function () {
-    // Rota para a página de perfil
-    Route::get('/profile', [ProfileController::class, 'show'])->name('profile.show');
-});
-
-Route::get('/calculadora-dano', [DamageCalculatorController::class, 'index'])->name('calculadora.dano');
-
-// Rotas para novas funcionalidades
-Route::middleware('auth')->group(function () {
-    // Rota para gerenciamento de narradores (somente admin)
-    Route::get('/admin/gerenciar-role', [AdminController::class, 'manageRole'])->name('admin.gerenciar_role');
-
-    // Rota para bestiário (gerenciamento de monstros/NPCs)
-    Route::get('/bestiario', [MonsterController::class, 'index'])->name('bestiario.index');
-
-    // Rota para exibir o formulário de criação de monstro/NPC
-    Route::get('/bestiario/criar', [MonsterController::class, 'create'])->name('bestiario.create');
-
-    // Rota para salvar o monstro/NPC
-    Route::post('/bestiario', [MonsterController::class, 'store'])->name('bestiario.store');
-
-    // Rota para editar um monstro/NPC
-    Route::get('/bestiario/{monster}/editar', [MonsterController::class, 'edit'])->name('bestiario.edit');
-
-    // Rota para atualizar um monstro/NPC
-    Route::put('/bestiario/{monster}', [MonsterController::class, 'update'])->name('bestiario.update');
-
-    // Rota para as missões
-    Route::get('/gerencia-missoes', [MissionController::class, 'index'])->name('misson.index');
-
-    Route::get('/missao/criar', [MissionController::class, 'criar'])->name('misson.create');
-
-    // Alterar a rota para aceitar POST para a criação da missão
-    Route::post('/missao', [MissionController::class, 'store'])->name('misson.store');
-
-    Route::get('/calculadora-dano', [DamageCalculatorController::class, 'index'])->name('calculadora.dano');
-
-    Route::get('/regras-rpg', [DamageCalculatorController::class, 'regras'])->name('calculadora.regras');
-
-    // Rota para exibir a tela inicial (Title Screen)
-    Route::get('/menuinicial', [WorldController::class, 'titleScreen'])->name('world.titlescreen');
-
-    // Rota para seleção de mundo
-    Route::get('/SolarSystem', [WorldController::class, 'sistemasolar'])->name('world.systemsun');
-
-    // Rota para exibir a introdução do jogo
-    Route::get('/introducao', [WorldController::class, 'introducao'])->name('world.introducao');
-
-    // Rota para exibir todas as regiões
-    Route::get('/world', [WorldController::class, 'index'])->name('world.index');
-
-    // Rota para exibir detalhes de uma região
-    Route::get('/world/region/{id}', [WorldController::class, 'show'])->name('world.region');
-
-    // Rota para exibir detalhes do hospital da região
-    Route::get('/world/region/{id}/hospital', [WorldController::class, 'hospital'])->name('world.hospital');
-
-    Route::post('/hospital/{id}/comprar/{item_id}', [WorldController::class, 'buyPotion'])->name('hospital.comprar');
-
-    // Rota para exibir detalhes da guilda da região
-    Route::get('/world/region/{id}/guild', [WorldController::class, 'showGuild'])->name('world.guild');
-
-    // Rota para exibir detalhes da loja da região
-    Route::get('/world/region/{id}/store', [WorldController::class, 'store'])->name('world.store');
-
-});
-
