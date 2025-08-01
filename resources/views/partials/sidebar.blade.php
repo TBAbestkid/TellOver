44d9811706ff04d15ae8eaec9082e7f9ef93331a
Kharen Aguiar <149444184+TBAbestkid@users.noreply.github.com>
2025-08-01 14:16:42 -0300

    Uma Sidebar simples e basica, apenas para testes visuais

---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,16 @@
+<div class="card mb-3">
+    <div class="card-body">
+        <ul class="nav flex-column">
+            <li class="nav-item"><a class="nav-link" href="#">🏠 Início</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">👤 Perfil</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">🔍 Explorar</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">⚙️ Configurações</a></li>
+            <!-- resources/views/partials/button_post.blade.php -->
+            <li class="nav-item">
+                <a href="#" class="nav-link" data-bs-toggle="modal" data-bs-target="#postModal">
+                    <i class="bi bi-pencil-square me-1"></i> Novo Post
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
