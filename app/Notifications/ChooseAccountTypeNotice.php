77c3894e10915d01ffd2d06065ac5998aeeea76a
Kharen Aguiar <149444184+TBAbestkid@users.noreply.github.com>
2025-08-04 14:40:35 -0300

    Criando nova notificação para usuario definir sua conta

---
+++ app/Notifications/ChooseAccountTypeNotice.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ChooseAccountTypeNotice extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['database'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+                    ->line('The introduction to the notification.')
+                    ->action('Notification Action', url('/'))
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public function toDatabase($notifiable): array
+    {
+        return [
+            'message' => "Atualize sua conta para escolher se deseja ser um Criador de Conteúdo ou apenas um Usuário Comum.",
+            'link' => route('account.settings'), // Altere conforme sua rota
+        ];
+    }
+
+}
