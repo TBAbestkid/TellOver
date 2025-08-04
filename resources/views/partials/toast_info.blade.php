ce153b0b1089afbb69969f0b41c7dd3f51b1adb7
Kharen Aguiar <149444184+TBAbestkid@users.noreply.github.com>
2025-08-04 14:08:46 -0300

    Create toast_info.blade.php

---
+++ resources/views/partials/toast_info.blade.php
@@ -0,0 +1,11 @@
+{{-- toast info --}}
+<div class="toast-container position-fixed bottom-0 end-0 p-3">
+    <div id="toastInfo" class="toast align-items-center text-white bg-info border-0" role="alert" aria-live="assertive" aria-atomic="true">
+        <div class="d-flex">
+            <div class="toast-body">
+                {{ $message }}
+            </div>
+            <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+        </div>
+    </div>
+</div>
